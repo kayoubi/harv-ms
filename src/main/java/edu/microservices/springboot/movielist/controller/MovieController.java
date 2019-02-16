22ad79032ee7b205c4a2208f20f75635161b4247
kayoubi <khaled.ayoubi@gmail.com>
2019-02-15 22:32:21 -0800

    basic form to create new movies and update the list

--- src/main/java/edu/microservices/springboot/movielist/controller/MovieController.java
+++ src/main/java/edu/microservices/springboot/movielist/controller/MovieController.java
@@ -1,5 +1,6 @@
 package edu.microservices.springboot.movielist.controller;
 
+import edu.microservices.springboot.movielist.model.Movie;
 import edu.microservices.springboot.movielist.model.MovieBuilder;
 import edu.microservices.springboot.movielist.service.MovieService;
 import org.slf4j.Logger;
@@ -7,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Random;
 
@@ -29,13 +32,23 @@ public class MovieController {
 
     @GetMapping("/movieList")
     public String movieList(Model model){
-        logger.info("created {}", movieService.create(MovieBuilder
-            .builder()
-            .withTitle("foobar " + new Random().nextInt())
-            .createMovie()).getId()
-        );
+//        logger.info("created {}", movieService.create(MovieBuilder
+//            .builder()
+//            .withTitle("foobar " + new Random().nextInt())
+//            .createMovie()).getId()
+//        );
+
         model.addAttribute("count", movieService.getAll().size());
         model.addAttribute("movies", movieService.getAll());
+        model.addAttribute("movie", new Movie());
+
         return "movieList";
+    }
+
+    @PostMapping("/createMovie")
+    public String create(@ModelAttribute Movie movie, Model model) {
+        movieService.create(movie);
+
+        return "redirect:/movieList";
     }
 }
--- src/main/java/edu/microservices/springboot/movielist/model/Movie.java
+++ src/main/java/edu/microservices/springboot/movielist/model/Movie.java
@@ -38,23 +38,47 @@ public class Movie {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public int getYear() {
         return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
     public String getDirector() {
         return director;
     }
 
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
     public String getStar() {
         return star;
+    }
+
+    public void setStar(String star) {
+        this.star = star;
     }
 
     public String getViewer() {
         return viewer;
     }
 
+    public void setViewer(String viewer) {
+        this.viewer = viewer;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
