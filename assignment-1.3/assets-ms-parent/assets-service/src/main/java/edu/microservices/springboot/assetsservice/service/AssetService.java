fca49c7dbd2cf491e09e59e4938f93b9ca9864bd
kayoubi <khaled.ayoubi@gmail.com>
2019-02-23 19:03:05 -0800

    reading properties from config server

--- assignment-1.3/assets-ms-parent/assets-service/src/main/java/edu/microservices/springboot/assetsservice/service/AssetService.java
+++ assignment-1.3/assets-ms-parent/assets-service/src/main/java/edu/microservices/springboot/assetsservice/service/AssetService.java
@@ -2,6 +2,7 @@ package edu.microservices.springboot.assetsservice.service;
 
 import edu.microservices.springboot.assetsservice.model.Asset;
 import edu.microservices.springboot.assetsservice.model.AssetBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,11 +10,14 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AssetService {
+    @Value("${spring.jpa.database}")
+    private String db;
+
     public Asset getAsset(String organizationId, String assetId) {
         return AssetBuilder.instance()
             .withId(assetId)
             .withAssetName("new Asset")
-            .withAssetType("random type")
+            .withAssetType(db)
             .withOrganizationId(organizationId)
             .build();
     }
