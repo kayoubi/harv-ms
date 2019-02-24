d63c709c3f7f2115330c36db86f712948569dc1c
kayoubi <khaled.ayoubi@gmail.com>
2019-02-23 17:35:11 -0800

    config server

---
+++ assignment-1.3/assets-ms-parent/cnfsrv/src/main/java/edu/microservices/springboot/cnfsrv/CnfsrvApplication.java
@@ -0,0 +1,13 @@
+package edu.microservices.springboot.cnfsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnfsrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnfsrvApplication.class, args);
+	}
+
+}
