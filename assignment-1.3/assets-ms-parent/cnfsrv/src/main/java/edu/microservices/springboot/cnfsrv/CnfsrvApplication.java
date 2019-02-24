ab811495b84cf7ef2a743800b009eb518115af29
kayoubi <khaled.ayoubi@gmail.com>
2019-02-23 18:35:45 -0800

    added configuration to the config server

--- assignment-1.3/assets-ms-parent/cnfsrv/src/main/java/edu/microservices/springboot/cnfsrv/CnfsrvApplication.java
+++ assignment-1.3/assets-ms-parent/cnfsrv/src/main/java/edu/microservices/springboot/cnfsrv/CnfsrvApplication.java
@@ -2,8 +2,10 @@ package edu.microservices.springboot.cnfsrv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CnfsrvApplication {
 
 	public static void main(String[] args) {
