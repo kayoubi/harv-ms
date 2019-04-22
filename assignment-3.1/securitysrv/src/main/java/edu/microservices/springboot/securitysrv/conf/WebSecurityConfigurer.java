61f4f5e1f0a7c8a6122736e56e8c1e20a848296f
khaled <khaled.ayoubi@gmail.com>
2019-04-21 19:15:40 -0700

    Getting access token to work

--- assignment-3.1/securitysrv/src/main/java/edu/microservices/springboot/securitysrv/conf/WebSecurityConfigurer.java
+++ assignment-3.1/securitysrv/src/main/java/edu/microservices/springboot/securitysrv/conf/WebSecurityConfigurer.java
@@ -3,8 +3,10 @@ package edu.microservices.springboot.securitysrv.conf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author khaled
@@ -24,4 +26,28 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsServiceBean() throws Exception {
         return super.userDetailsServiceBean();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("john.carnell").password("password1").roles("USER")
+            .and()
+            .withUser("william.woodward").password("password2").roles("USER", "ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return rawPassword.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return true;
+            }
+        };
+    }
+
 }
